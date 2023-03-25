1c5674ef41cf3853342e80516283f175ac43bce0
yanagimotoK <121187767+yanagimotoK@users.noreply.github.com>
2023-03-26 02:00:40 +0900

    3章提出課題

---
+++ PHPjob/3-1/index.php
@@ -0,0 +1,18 @@
+<?php
+for ($i=1; $i <= 100; $i++) { 
+    if ($i % 3===0 && $i % 5===0) {
+        echo "FizzBuzz";
+        echo "<br>";
+    }elseif ($i % 5===0) {
+        echo "Buzz";
+        echo "<br>";
+    }elseif($i % 3===0){
+        echo "Fizz";
+        echo "<br>";
+    } else{
+        echo $i;
+        echo "<br>";
+    }
+}
+
+?>
